2cd4985b35d0bd2d167bbe083c92689a4ade1500
emilschultz <emil.schultz@amedia.no>
2024-02-27 22:04:41 +0100

    feat: update and delete functionality frontend

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,93 @@
 'use client';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Post = ({ post, setPosts }) => {
   const { _id, postedAt, body, user: foodieUser } = post;
   const [deleted, setDeleted] = useState(false);
+  const [inputDisabled, setInputDisabled] = useState(false);
+  const [modalOpened, setModalOpened] = useState(false);
+
+  const { register, handleSubmit, reset, setValue } = useForm({
+    defaultValues: {
+      post: '',
+    },
+  });
+
+  const editPost = () => {
+    setValue('editPost', body);
+    setModalOpened(true);
+  };
+
+  const onUpdatePost = async (value) => {
+    setInputDisabled(true);
+    const response = await fetch('/api/post', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        _id,
+        body: value.editPost,
+      }),
+    });
+
+    const responseJson = await response.json();
+    console.log(responseJson);
+
+    setPosts((posts) =>
+      posts.map((post) => {
+        if (post._id === _id) {
+          return {
+            ...post,
+            body: value.editPost,
+          };
+        }
+
+        return post;
+      })
+    );
+
+    reset();
+    setInputDisabled(false);
+    setModalOpened(false);
+    alert('Your post has been updated');
+  };
+
+  const deletePost = async () => {
+    const response = await fetch(`/api/post/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        _id,
+      }),
+    });
+    const responseJson = await response.json();
+    console.log(responseJson);
+    setDeleted(true);
+    alert('Your post has been DELETED  💀');
+  };
 
   return (
     <>
       {!deleted && (
         <>
           <div>
+            <form onSubmit={handleSubmit((value) => onUpdatePost(value))}>
+              <textarea
+                required
+                data-autofocus
+                placeholder='Edit your post.'
+                variant='filled'
+                {...register('editPost')}
+              />
+              <button type='submit' disabled={inputDisabled}>
+                Update
+              </button>
+            </form>
+
             <div>
               <img src={foodieUser.picture} alt={foodieUser.name} />
               <div>
@@ -22,8 +100,8 @@ const Post = ({ post, setPosts }) => {
               <div>
                 <p>0 people liked this</p>
                 <div>
-                  <button onClick={() => editFlutter()}>Edit</button>
-                  <button onClick={() => deleteFlutter()}>Delete</button>
+                  <button onClick={() => editPost()}>Edit</button>
+                  <button onClick={() => deletePost()}>Delete</button>
                 </div>
               </div>
             </div>
