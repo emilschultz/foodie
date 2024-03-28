f3baeba6bc4c4f3c7acfce8e20d6c966409cf3fc
emilschultz <emil.schultz@amedia.no>
2024-03-28 14:30:26 +0100

    feat: initial follow api

---
+++ pages/api/user/follow/index.js
@@ -0,0 +1,47 @@
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+
+export default withApiAuthRequired(async function handler(req, res) {
+  const { accessToken } = await getAccessToken(req, res);
+  const fetchOptions = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      jwtTokenString: accessToken,
+    },
+  };
+  const fetchBody = {
+    dataSource: process.env.MONGODB_DATA_SOURCE,
+    database: 'foodie',
+    collection: 'users',
+  };
+  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+
+  try {
+    switch (req.method) {
+      case 'PUT':
+        const action = req.body.action === 'follow' ? '$addToSet' : '$pull';
+        const updateData = await fetch(`${baseUrl}/updateOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            filter: { _id: { $oid: req.body._id } },
+            update: {
+              [req.body.action]: {
+                followers: req.body.userId,
+              },
+            },
+          }),
+        });
+        const updateDataJson = await updateData.json();
+        res.status(200).json(updateDataJson);
+        break;
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
