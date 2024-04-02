7d452ed361f15e8846ba1b45149dbb62e99c2843
emilschultz <emilrschultz@gmail.com>
2024-04-02 21:38:20 +0200

    refactor: render posts with Next Link

--- components/Posts/Posts.js
+++ components/Posts/Posts.js
@@ -1,13 +1,16 @@
 import Post from '../Post/Post.js';
+import Link from 'next/link';
 import styles from './Posts.module.css';
 
 const Posts = ({ posts, setPosts }) => {
   return (
     <>
       {posts.map((post) => (
-        <div key={post._id} className={styles.container}>
-          <Post post={post} setPosts={setPosts} />
-        </div>
+        <li key={post._id} className={styles.container}>
+          <Link post={post} href={`/recipe/${encodeURIComponent(post._id)}`}>
+            <Post post={post} setPosts={setPosts} />
+          </Link>
+        </li>
       ))}
     </>
   );
