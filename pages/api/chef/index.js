36acb96bfcad46656ed4c29b761bf0fc6b9b086f
emilschultz <emilrschultz@gmail.com>
2024-04-08 18:49:21 +0200

    get post creator api

---
+++ pages/api/chef/index.js
@@ -0,0 +1,47 @@
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+
+export default withApiAuthRequired(async function handler(req, res) {
+  const { accessToken } = await getAccessToken(req, res);
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      jwtTokenString: accessToken,
+    },
+  };
+
+  const fetchBody = {
+    dataSource: process.env.MONGODB_DATA_SOURCE,
+    database: 'foodie',
+    collection: 'posts',
+  };
+
+  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+
+  try {
+    switch (req.method) {
+      case 'GET':
+        const userId = req.query.userId;
+        const readData = await fetch(`${baseUrl}/find`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            filter: {
+              'user.$oid': userId,
+            },
+          }),
+        });
+        const readDataJson = await readData.json();
+        res.status(200).json(readDataJson.documents[0]);
+        break;
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+});
