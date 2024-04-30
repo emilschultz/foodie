478a7537c345d4a8f1eb7a229ee66b2b00567e99
emilschultz <emilrschultz@gmail.com>
2024-04-30 10:49:49 +0200

    dynamic render chef profile page

--- pages/chef/[chefId].js
+++ pages/chef/[chefId].js
@@ -1,46 +1,43 @@
 import Navbar from '../../components/Navbar/Navbar.js';
-// import ProfilePosts from '../../components/ProfilePosts/ProfilePosts.js';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import ChefProfile from '../../components/chefProfile/ChefProfile.js';
 
 export default function ChefPage() {
   const router = useRouter();
-  const [userData, setUserData] = useState(null);
+  const [user, setUser] = useState(null);
+  const foodieUser = user;
 
   useEffect(() => {
     if (router.query.chefId) {
       (async () => {
         try {
-          // Fetch post data
-          const getIdFromPost = await fetch(
+          const getUser = await fetch(
             `/api/chef?chefId=${router.query.chefId}`
           );
-          const postData = await getIdFromPost.json();
-          // setChef(postData);
-          console.log('post data:', postData);
-
-          // Fetch user data
-          const getUser = await fetch(`/api/user?chefId=${postData.user.id}`);
-          const userData = await getUser.json();
-          setUserData(userData);
+          const getUserJson = await getUser.json();
+          console.log(getUserJson);
+          setUser(getUserJson);
         } catch (error) {
-          console.error('Error fetching data:', error);
+          console.error('Error fetching chef with ID:', error);
         }
       })();
     }
   }, [router.query.chefId]);
 
+  const { document } = user || {};
+  const { nickname, picture, posts, id } = document || {};
+
   return (
     <>
-      {userData && (
-        <div>
-          <h1>User Profile</h1>
-          <p>Name: {userData.name}</p>
-          <p>Email: {userData.email}</p>
-          <p>Other User Data...</p>
-        </div>
+      {user !== null && (
+        <ChefProfile
+          nickname={nickname}
+          picture={picture}
+          posts={posts}
+          id={id}
+        />
       )}
-      {/* <ProfilePosts posts={} setPosts={} /> */}
 
       <Navbar />
     </>
