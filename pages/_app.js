4c929ae6648f1bd8b12cafbb2ce40f4f7468dada
emilschultz <emilrschultz@gmail.com>
2024-08-19 22:31:40 +0200

    clean up mongo anf firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
 'use client';
 import Head from 'next/head';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { UserProvider as AtlasUserProvider } from '../context/UserContext.js';
-import { initializeApp } from 'firebase/app';
+import { AuthProvider } from '../context/authContexts.js'
 import styles from './page.module.css';
 import './globals.css';
 
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID, 
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); 
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -29,11 +15,9 @@ export default function App({ Component, pageProps }) {
         />
       </Head>
       <main className={styles.main}>
-        <UserProvider>
-          <AtlasUserProvider>
+        <AuthProvider>
             <Component {...pageProps} />
-          </AtlasUserProvider>
-        </UserProvider>
+        </AuthProvider>
       </main>
     </>
   );
