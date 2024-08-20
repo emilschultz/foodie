bf3694e5b8d9c3e880e0f1159e6c437cab6fe767
emilschultz <emilrschultz@gmail.com>
2024-08-20 21:44:35 +0200

    handle signup flow

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -5,6 +5,7 @@ import { signUp } from '../../lib/auth';
 export default function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [nickname, setNickname] = useState('');
   const [error, setError] = useState(null);
   const router = useRouter();
 
@@ -13,22 +14,45 @@ export default function SignUp() {
     setError(null);
 
     try {
-      await signUp(email, password);
-      router.push('/')
+      const additionalData = {
+        nickname: nickname,
+      };
+      // Sign up the user with email and password, and store additional data in Firestore
+      await signUp(email, password, additionalData);
+      router.push('/'); 
     } catch (error) {
       setError(error.message);
     }
   };
 
   return (
-    <>
-    <h1>Sign Up</h1>
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-      {error && <p>{error}</p>}
-      <button type="submit">Sign Up</button>
-    </form>
-    </>
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <input
+          type="text"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          placeholder="Nickname"
+          required
+        />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 }
