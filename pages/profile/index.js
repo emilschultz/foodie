2fb596082220df730bf8f0252abcdee4ff440c7a
emilschultz <emilrschultz@gmail.com>
2024-08-21 22:49:38 +0200

    dev

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -32,7 +32,7 @@ const Profile = () => {
           console.error('Error fetching user data:', error);
         }
       } else {
-        router.push('/signin');
+        // router.push('/signin');
       }
     };
 
@@ -47,37 +47,44 @@ const Profile = () => {
       console.error('Error signing out:', error);
     }
   };
-  console.log(userData);
 
   return (
     <>
       {userData ? (
-        <section className={styles.bio}>
-          {/* {userData.profilePicture && <img src={userData.profilePicture} alt="Profile" />} */}
-          <img className={styles.picture} src='https://picsum.photos/100'/>
-          <div className={styles.names}>
-          <p className={styles.name}>{userData.firstname} {userData.lastname}</p>
-          <p className={styles.nickname}>@{userData.nickname}</p>
-          </div>
-          <div className={styles.statscontainer}>
-            <div className={styles.stats}>
-              <p className={styles.statNumber} >{userData.followers ? userData.followers.length : 2883}</p>
-              <p className={styles.statTitle}>Followers</p> 
+        <>
+          <section className={styles.bio}>
+            {/* {userData.profilePicture && <img src={userData.profilePicture} alt="Profile" />} */}
+            <img className={styles.picture} src='https://picsum.photos/100'/>
+            <div className={styles.names}>
+            <p className={styles.name}>{userData.firstname} {userData.lastname}</p>
+            <p className={styles.nickname}>@{userData.nickname}</p>
             </div>
-            
-            <div className={styles.stats}>
-            <p className={styles.statNumber}>{userData.following.length > 0 ? userData.following.length : 354}</p>
-            <p className={styles.statTitle}>Following</p>
-            </div>
+            <div className={styles.statscontainer}>
+              <div className={styles.stats}>
+                <p className={styles.statNumber} >{userData.followers ? userData.followers.length : 2883}</p>
+                <p className={styles.statTitle}>Followers</p> 
+              </div>
+              
+              <div className={styles.stats}>
+              <p className={styles.statNumber}>{userData.following.length > 0 ? userData.following.length : 354}</p>
+              <p className={styles.statTitle}>Following</p>
+              </div>
 
-            <div className={styles.stats}>
-            <p className={styles.statNumber}>{userData.posts.length > 0 ? userData.posts.length : 172}</p>
-            <p className={styles.statTitle}>Recipes</p>
-            </div>
+              <div className={styles.stats}>
+              <p className={styles.statNumber}>{userData.posts.length > 0 ? userData.posts.length : 172}</p>
+              <p className={styles.statTitle}>Recipes</p>
+              </div>
 
-          </div>
-          <button className={styles.followBtn}>Follow</button>
-        </section>
+            </div>
+            {/* Follow knappen skal egentlig ikke være her, men på chefProfile. Dette er jo ens egen profilside */}
+            <button className={styles.followBtn}>Follow</button>
+          </section>
+
+          <section>
+            <h3>Posts</h3>
+            <p>Posts here</p>
+          </section>
+        </>
 
       ) : (
         <p>Loading...</p>
