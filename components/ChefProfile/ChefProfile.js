9e818a216c0d4267078c6a5b8652224c28ac1a2d
emilschultz <emilrschultz@gmail.com>
2024-09-21 12:47:18 +0200

    refactor: use reusable RecipeList component to render out recipes

--- components/ChefProfile/ChefProfile.js
+++ components/ChefProfile/ChefProfile.js
@@ -1,8 +1,7 @@
+import RecipeList from '../RecipeList/RecipeList.js'
 import styles from './ChefProfile.module.css'; 
 
 export const ChefProfile = ({ posts, nickname, picture, followers }) => {
-  
-  
   return (
     <>
       <section className={styles.bio}>
@@ -13,34 +12,11 @@ export const ChefProfile = ({ posts, nickname, picture, followers }) => {
           <p>Followers {followers ? followers.length : 0}</p>
           <p>Likes {}</p>
         </div>
-        {/* <button onClick={follow}>Follow</button> */}
+        <button className={styles.followBtn}>Follow</button>
+
       </section>
       <section className={styles.grid}>
-        {posts.map(
-          (post) => (
-            (
-              <div key={post.postedAt} className={styles.post}>
-                {post.media &&
-                  (post.media.type === 'image/jpg' ||
-                    post.media.type === 'image/jpeg' ||
-                    post.media.type === 'image/png' ||
-                    post.media.type === 'image/avif') && (
-                    <img src={post.media.url} className={styles.image} />
-                  )}
-
-                {post.media &&
-                  (post.media.type === 'video/quicktime' ||
-                    post.media.type === 'video/mp4' ||
-                    post.media.type === 'video/ogg') && (
-                    <video autoPlay muted loop controls>
-                      <source src={post.media.url} type={post.media.type} />
-                      Your browser does not support the video tag.
-                    </video>
-                  )}
-              </div>
-            )
-          )
-        )}
+        <RecipeList recipes={posts} />
       </section>
     </>
   );
