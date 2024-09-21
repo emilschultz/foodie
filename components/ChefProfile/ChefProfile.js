580b5bf25dd4ec6ca7bb998558e8c5b257571004
emilschultz <emilrschultz@gmail.com>
2024-09-21 12:31:34 +0200

    clean out

--- components/ChefProfile/ChefProfile.js
+++ components/ChefProfile/ChefProfile.js
@@ -1,7 +1,6 @@
-import { useState } from 'react';
 import styles from './ChefProfile.module.css'; 
 
-export const ChefProfile = ({ posts, nickname, picture, id, followers }) => {
+export const ChefProfile = ({ posts, nickname, picture, followers }) => {
   
   
   return (
