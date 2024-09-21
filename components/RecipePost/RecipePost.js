4fadc23ad653ba3741e81900f777eb0dd1c7d96a
emilschultz <emilrschultz@gmail.com>
2024-09-21 12:47:43 +0200

    conditionally render the post header

--- components/RecipePost/RecipePost.js
+++ components/RecipePost/RecipePost.js
@@ -4,6 +4,7 @@ import styles from './RecipePost.module.css';
 const RecipePost = ({ id, media, title, user }) => {
   const { name, nickname, uid, picture } = user || {};
   const router = useRouter();
+  const isDiscoverPage = router.pathname=== '/discover';
 
   const handleProfileClick = () => {
     if(uid) {
@@ -17,28 +18,29 @@ const RecipePost = ({ id, media, title, user }) => {
     }
   }
 
-
-
   return (
     <div key={id} className={styles.post}>
-      <div className={styles.header} onClick={handleProfileClick}>
-        <img
-          src={picture || 'https://via.placeholder.com/150'}
-          alt="User profile pitcure for"
-          className={styles.picture}
-        />
-        @{nickname || name || 'name'}
-      </div>
-      <div className={styles.content} onClick={handleRecipeClick}>
-      {media &&
-        (media.type === 'image/jpg' ||
-          media.type === 'image/jpeg' ||
-          media.type === 'image/png' ||
-          media.type === 'image/avif') && (
-          <img src={media.url} alt="Recipe" className={styles.image} />
-        )}
-      {title}
-      </div>
+
+      {isDiscoverPage && <div className={styles.header} onClick={handleProfileClick}>
+          <img
+            src={picture || 'https://via.placeholder.com/150'}
+            alt="User profile pitcure for"
+            className={styles.picture}
+          />
+          @{nickname || name || 'name'}
+        </div>
+        }
+
+        <div className={styles.content} onClick={handleRecipeClick}>
+        {media &&
+          (media.type === 'image/jpg' ||
+            media.type === 'image/jpeg' ||
+            media.type === 'image/png' ||
+            media.type === 'image/avif') && (
+            <img src={media.url} alt="Recipe" className={styles.image} />
+          )}
+        {title}
+        </div>
     </div>
   );
 };
