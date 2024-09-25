39fd16c845bac63cd71bf069c008614b00b8c02a
emilschultz <emilrschultz@gmail.com>
2024-09-25 22:42:43 +0200

    configuration for next/image. remotePatterns holds trusted hostnames fo r image srces

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '', // Leave blank if not needed
+        pathname: '/**', // Matches all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '', // Leave blank if not needed
+        pathname: '/**', // Matches all paths
+      },
+    ],
+  }
+};
 
 export default nextConfig;
